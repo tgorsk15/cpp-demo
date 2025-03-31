91edfaea05407f5b47c8e07b893232c4ff951d6e
tgorsk15 <tgbusiness463@gmail.com>
2025-03-31 10:40:08 -0500

    Add printDouble() to practice arguement passing

--- demo.cpp
+++ demo.cpp
@@ -13,12 +13,17 @@ int getValue()
     return input;
 }
 
+void printDouble(int num)
+{
+    std::cout << num << " doubled is " << num * 2 << '\n';
+}
+
 int main()
 {
 
-    int num = getValue();
+    int response = getValue();
     
-    std::cout << num << " doubled is " << num * 2 << '\n';
+    printDouble(response);
 
     return 0;
 }
